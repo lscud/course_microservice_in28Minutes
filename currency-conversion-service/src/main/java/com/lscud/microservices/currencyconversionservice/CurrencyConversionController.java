662685c120eea7a8aaee7e212cf84bfa36634de0
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-06-12 14:41:34 -0300

    Using Feign REST Client for Service Invocation

--- currency-conversion-service/src/main/java/com/lscud/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/lscud/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.lscud.microservices.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -31,7 +35,22 @@ public class CurrencyConversionController {
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
-                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment() + " " + "Rest Template");
+
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable(value="from") String from,
+            @PathVariable(value="to") String to,
+            @PathVariable(value="quantity") BigDecimal quantity
+
+    ){
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from,to);
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment() + " " + "Feign");
 
     }
 }
