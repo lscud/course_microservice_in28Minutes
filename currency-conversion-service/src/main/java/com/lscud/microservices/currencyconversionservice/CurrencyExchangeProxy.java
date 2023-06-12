1a83b0daa117b57872f52de72f69f56d98191553
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-06-12 16:39:56 -0300

    Load Balancing with Eureka, Feign & Spring Cloud LoadBalancer

--- currency-conversion-service/src/main/java/com/lscud/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/lscud/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange") //apenas dessa forma este microserviço irá consultar o naming-server para saber qual instancia do currency-exchange está diponivel.
 public interface CurrencyExchangeProxy {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
