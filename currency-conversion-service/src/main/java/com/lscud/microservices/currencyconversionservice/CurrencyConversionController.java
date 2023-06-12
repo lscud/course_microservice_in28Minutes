f7d83112d3b22c658761741d9c6ebece1addaeb3
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-06-12 14:07:36 -0300

    first commit

---
+++ currency-conversion-service/src/main/java/com/lscud/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,37 @@
+package com.lscud.microservices.currencyconversionservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable(value="from") String from,
+            @PathVariable(value="to") String to,
+            @PathVariable(value="quantity") BigDecimal quantity
+
+    ){
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to",to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+        //        Alternativamente:
+//        String.format("http://localhost:8080/stock/%s", id)
+//        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(String.format("http://localhost:8000/currency-exchange/from/%s/to/%s",from,to), CurrencyConversion.class);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+
+    }
+}
