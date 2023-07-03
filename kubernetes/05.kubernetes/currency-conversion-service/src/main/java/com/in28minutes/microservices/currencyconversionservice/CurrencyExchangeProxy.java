eb7c7d8a2bee2717b7b3dfd6878cb7f7ab6273b5
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-07-03 09:37:42 -0300

    Kubernets - Changing var CURRENCY_EXCHANGE_SERVICE_HOST (proper to kubernetes) to CURRENCY_EXCHANGE_URI (custom) and change tag to deploy new imagem (pom)

--- kubernetes/05.kubernetes/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ kubernetes/05.kubernetes/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name="currency-exchange")
 
 //CHANGE-KUBERNETES
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000") //se a variavel de ambiente CURRENCY_EXCHANGE_SERVICE_HOST nao existir aí será usado localhost. Essa variavel é usada no kubernets e o lcoal host no ambiente local
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000") //se a variavel de ambiente CURRENCY_EXCHANGE_SERVICE_HOST nao existir aí será usado localhost. Essa variavel é usada no kubernets e o lcoal host no ambiente local
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000") // Fazemos isso pois caso o currency-exchange fique Down a VAR fornecida pelo kubernets - CURRENCY_EXCHANGE_SERVICE_HOST ficará indisponível. PAra resolver esta situação criamos uma variável nossa - CURRENCY_EXCHANGE_URI
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
